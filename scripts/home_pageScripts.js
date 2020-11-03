8e6d70fa8fab7b122380b6073daee4f180437c85
Gonçalo <goncariaferreira@gmail.com>
2020-11-03 21:32:05 +0000

    loginjs=home_pagejs

---
+++ scripts/home_pageScripts.js
@@ -0,0 +1,37 @@
+//Grupo:003, Nomes: Francisco Pimenta - 54973, Pedro Quintão - 54971, Miguel Duarte - 54941, Gonçalo Ferreira - 55166
+
+function loginDimmerShower() {
+    let backgroundDimmer = document.getElementsByClassName("backgroundPopups")[0];
+    let loginPopup = document.getElementsByClassName("loginDimmer")[0];
+    loginPopup.style.display = "block";
+    backgroundDimmer.style.opacity = "1";
+}
+
+
+function loginDimmerCloser() {
+    let backgroundDimmer = document.getElementsByClassName("backgroundPopups")[0];
+    let loginPopup = document.getElementsByClassName("loginDimmer")[0];
+    loginPopup.style.display = "none";
+    backgroundDimmer.style.opacity = "0";
+
+
+
+}
+
+
+
+
+function RegistarDimmerShower() {
+    let backgroundDimmer = document.getElementsByClassName("backgroundPopups")[0];
+    let registarPopup = document.getElementsByClassName("registarDimmer")[0];
+    registarPopup.style.display = "block";
+    backgroundDimmer.style.opacity = "1";
+}
+
+
+function RegistarDimmerCloser() {
+    let backgroundDimmer = document.getElementsByClassName("backgroundPopups")[0];
+    let registarPopup = document.getElementsByClassName("registarDimmer")[0];
+    registarPopup.style.display = "none";
+    backgroundDimmer.style.opacity = "0";
+}
