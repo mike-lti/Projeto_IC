c7af3b5d7e61c320f26b59701529edfde6044477
quintons14 <miguelquintao14@gmail.com>
2020-11-04 20:40:35 +0000

    popup album

--- scripts/album_pageScripts.js
+++ scripts/album_pageScripts.js
@@ -1,13 +1,13 @@
 //Grupo:003, Nomes: Francisco Pimenta - 54973, Pedro Quintão - 54971, Miguel Duarte - 54941, Gonçalo Ferreira - 55166
 
 function showModal() {
-    document.getElementsByClassName("importing_modal")[0].style.display = "inline-block";
-    document.getElementsByClassName("importing_modal")[0].style.zIndex = "9";
+    document.getElementsByClassName("album_modal")[0].style.display = "inline-block";
+    document.getElementsByClassName("album_modal")[0].style.zIndex = "9";
     document.getElementsByClassName("dimmer")[0].style.opacity = "1";
 }
 
 function closeModal() {
-    document.getElementsByClassName("importing_modal")[0].style.display = "none";
+    document.getElementsByClassName("album_modal")[0].style.display = "none";
     document.getElementsByClassName("dimmer")[0].style.opacity = "0";
 }
 
