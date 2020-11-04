fb93385f5394c288b6104dbd3b2e8fd196dbbe3a
quintons14 <miguelquintao14@gmail.com>
2020-11-04 19:59:03 +0000

    dimmer album

---
+++ scripts/album_pageScripts.js
@@ -0,0 +1,24 @@
+//Grupo:003, Nomes: Francisco Pimenta - 54973, Pedro Quintão - 54971, Miguel Duarte - 54941, Gonçalo Ferreira - 55166
+
+function showModal() {
+    document.getElementsByClassName("importing_modal")[0].style.display = "inline-block";
+    document.getElementsByClassName("importing_modal")[0].style.zIndex = "9";
+    document.getElementsByClassName("dimmer")[0].style.opacity = "1";
+}
+
+function closeModal() {
+    document.getElementsByClassName("importing_modal")[0].style.display = "none";
+    document.getElementsByClassName("dimmer")[0].style.opacity = "0";
+}
+
+function importCubaPhotos() {
+
+}
+
+function importFrancePhotos() {
+
+}
+
+function importSpikePhotos() {
+    
+}
