78a9085854a197495d3162dc31f618420423a438
Francisco Miguel Mendes Pimenta <xisco.pimenta@gmail.com>
2020-11-04 16:49:51 +0000

    Popup Importar

--- scripts/importar_pageScripts.js
+++ scripts/importar_pageScripts.js
@@ -1,5 +1,6 @@
 //Grupo:003, Nomes: Francisco Pimenta - 54973, Pedro Quintão - 54971, Miguel Duarte - 54941, Gonçalo Ferreira - 55166
 
-function importPhotos() {
-    
+function showModal() {
+    document.getElementsByClassName("importing_modal")[0].style.display = "inline-block";
+    document.getElementsByClassName("importing_modal")[0].style.zIndex = "2";
 }
