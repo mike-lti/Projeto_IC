3ca65dc57aff89afe2ca540e34ebb0d6392b9030
FMPimenta <xisco.pimenta@gmail.com>
2020-11-11 14:08:02 +0000

    Minor Updates

---
+++ scripts/favoritos_pageScripts.js
@@ -0,0 +1,13 @@
+//Grupo:003, Nomes: Francisco Pimenta - 54973, Pedro Quintão - 54971, Miguel Duarte - 54941, Gonçalo Ferreira - 55166
+
+"use strict";
+
+$(document).ready(show_favoritos());
+
+let favList = localStorage.getItem("favoritos")
+
+function show_favoritos() {
+    for (let foto of favList) {
+
+    }
+}
--- scripts/partilhar_pageScripts.js
+++ scripts/partilhar_pageScripts.js
@@ -1,3 +1,7 @@
+//Grupo:003, Nomes: Francisco Pimenta - 54973, Pedro Quintão - 54971, Miguel Duarte - 54941, Gonçalo Ferreira - 55166
+
+"use strict";
+
 function openLinkPopup(){
     document.getElementsByClassName("dimmer")[0].style.opacity="1"
     document.getElementsByClassName("linkPopup")[0].style.display="block"
