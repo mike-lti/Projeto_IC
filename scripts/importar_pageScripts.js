b0432b059396203a5cb1d3449a3e8850cea9e398
mike12-debug <62181571+mike12-debug@users.noreply.github.com>
2020-11-13 18:34:55 +0000

    Local storage data with filtros and imgs
    Buedi mambos nessa lacal com meu puto JESON

--- scripts/importar_pageScripts.js
+++ scripts/importar_pageScripts.js
@@ -4,6 +4,13 @@
 
 var arrayImagensImportadas = []; 
 
+function filter(filter1, filter2) {
+    this.firstName = first;
+    this.lastName = last;
+    
+  }
+
+
 function showModal() {
     document.getElementsByClassName("importing_modal")[0].style.display = "inline-block";
     document.getElementsByClassName("importing_modal")[0].style.zIndex = "9";
@@ -31,22 +38,54 @@ function confirmSelection() {
 function selectCuba() {
     document.getElementById("botao-confirmar").disabled = false;
     localStorage.setItem("selectedFile", "Cuba");
+<<<<<<< Updated upstream
     var arrayImagensCuba = ["img_cuba/img_1.jpg", "img_cuba/img_2.jpg", "img_cuba/img_3.jpg", "img_cuba/img_4.jpg", "img_cuba/img_5.jpg", "img_cuba/img_6.jpg", "img_cuba/img_7.jpg", "img_cuba/img_8.jpg", "img_cuba/img_9.jpg", "img_cuba/img_10.jpg", "img_cuba/img_11.jpg", "img_cuba/img_12.jpg", "img_cuba/img_13.jpg", "img_cuba/img_14.jpg", "img_cuba/img_15.jpg", "img_cuba/img_16.jpg", "img_cuba/img_17.jpg", "img_cuba/img_18.jpg", "img_cuba/img_19.jpg", "img_cuba/img_20.jpg"];
    
     if (localStorage.getItem("imagensImportadas") == null) {
+=======
+    var arrayImagensCuba = ["img_cuba/img_1.jpg", "img_cuba/img_2.jpg", "img_cuba/img_3.jpg", "img_cuba/img_4.jpg",
+    "img_cuba/img_5.jpg", "img_cuba/img_6.jpg", "img_cuba/img_7.jpg", "img_cuba/img_8.jpg", "img_cuba/img_9.jpg",
+    "img_cuba/img_10.jpg", "img_cuba/img_11.jpg", "img_cuba/img_12.jpg", "img_cuba/img_13.jpg", "img_cuba/img_14.jpg",
+    "img_cuba/img_15.jpg", "img_cuba/img_16.jpg", "img_cuba/img_17.jpg", "img_cuba/img_18.jpg", "img_cuba/img_19.jpg", "img_cuba/img_20.jpg"];
+    var arrayTagsImg = [["true", "true"], ["true", "true"], ["false","true"], ["false", "false"], 
+    ["false", "true"], ["true", "true"], ["true", "true"], ["true", "true"]
+    , ["true", "true"], ["true", "true"], ["true", "true"], ["true", "true"]
+    , ["true", "true"], ["false", "true"], ["false", "true"], ["false", "true"],
+     ["true", "true"], ["false", "true"], ["false", "true"], ["true", "true"]];
+
+
+    var objArray = objHandler(arrayImagensCuba, arrayTagsImg);
+    
+
+    if (localStorage.getItem("imagensImportadas") == null) {      
+>>>>>>> Stashed changes
         arrayImagensImportadas.push(arrayImagensCuba);
+        localStorage.setItem("imagensFiltros", JSON.stringify(objArray));
         localStorage.setItem("imagensImportadas", JSON.stringify(arrayImagensImportadas));
-    } else {
+    }else {        
         arrayImagensImportadas = JSON.parse(localStorage.getItem("imagensImportadas"));
         arrayImagensImportadas.push(arrayImagensCuba);
         localStorage.setItem("imagensImportadas", JSON.stringify(arrayImagensImportadas));
     }
+
+    if(localStorage.getItem("imagensFiltros") == null) {        
+        localStorage.setItem("imagensFiltros", JSON.stringify(objArray));
+    }else{
+        var objCheck = JSON.parse(localStorage.getItem("imagensFiltros"));
+        objCheck.push(objArray);
+        localStorage.setItem("imagensFiltros", JSON.stringify(objCheck));
+    }
+
 }
 
 function selectFranca() {
     document.getElementById("botao-confirmar").disabled = false;
     localStorage.setItem("selectedFile", "Franca");
+<<<<<<< Updated upstream
     var arrayImagensFranca = ["img_franca/img_1.jpg", "img_franca/img_2.jpg", "img_franca/img_3.jpg", "img_franca/img_4.jpg", "img_franca/img_5.jpg", "img_franca/img_6.jpg", "img_franca/img_7.jpg", "img_franca/img_8.jpg", "img_franca/img_9.jpg", "img_franca/img_10.jpg", "img_franca/img_11.jpg", "img_franca/img_12.jpg", "img_franca/img_13.jpg", "img_franca/img_14.jpg", "img_franca/img_15.jpg", "img_franca/img_16.jpg", "img_franca/img_17.jpg", "img_franca/img_18.jpg", "img_franca/img_19.jpg", "img_franca/img_20.jpg"];
+=======
+    var arrayImagensFranca = ["img_franca/img1_.jpg", "img_franca/img_2.jpg", "img_franca/img_3.jpg", "img_franca/img_4.jpg", "img_franca/img_5.jpg", "img_franca/img_6.jpg", "img_franca/img_7.jpg", "img_franca/img_8.jpg", "img_franca/img_9.jpg", "img_franca/img_10.jpg", "img_franca/img_11.jpg", "img_franca/img_12.jpg", "img_franca/img_13.jpg", "img_franca/img_14.jpg", "img_franca/img_15.jpg", "img_franca/img_16.jpg", "img_franca/img_17.jpg", "img_franca/img_18.jpg", "img_franca/img_9.jpg", "img_franca/img_20.jpg"];
+>>>>>>> Stashed changes
     if (localStorage.getItem("imagensImportadas") == null) {
         arrayImagensImportadas.push(arrayImagensFranca);
         localStorage.setItem("imagensImportadas", JSON.stringify(arrayImagensImportadas));
@@ -61,7 +100,7 @@ function selectFranca() {
 function selectSpike() {
     document.getElementById("botao-confirmar").disabled = false;
     localStorage.setItem("selectedFile", "Spike");
-    var arrayImagensSpike = ["img_spike/img1_.jpg", "img_spike/img_2.jpg", "img_spike/img_3.jpg", "img_spike/img_4.jpg", "img_spike/img_5.jpg", "img_spike/img_6.jpg", "img_spike/img_7.jpg", "img_spike/img_8.jpg", "img_spike/img_9.jpg", "img_spike/img_10.jpg", "img_spike/img_11.jpg", "img_spike/img_12.jpg", "img_spike/img_13.jpg", "img_spike/img_14.jpg", "img_spike/img_15.jpg", "img_spike/img_16.jpg", "img_spike/img_17.jpg", "img_spike/img_18.jpg", "img_spike/img_9.jpg", "img_spike/img_20.jpg"];
+    var arrayImagensSpike = ["img_spike/img_1.jpg", "img_spike/img_2.jpg", "img_spike/img_3.jpg", "img_spike/img_4.jpg", "img_spike/img_5.jpg", "img_spike/img_6.jpg", "img_spike/img_7.jpg", "img_spike/img_8.jpg", "img_spike/img_9.jpg", "img_spike/img_10.jpg", "img_spike/img_11.jpg", "img_spike/img_12.jpg", "img_spike/img_13.jpg", "img_spike/img_14.jpg", "img_spike/img_15.jpg", "img_spike/img_16.jpg", "img_spike/img_17.jpg", "img_spike/img_18.jpg", "img_spike/img_9.jpg", "img_spike/img_20.jpg"];
     if (localStorage.getItem("imagensImportadas") == null) {
         arrayImagensImportadas.push(arrayImagensSpike);
         localStorage.setItem("imagensImportadas", JSON.stringify(arrayImagensImportadas));
@@ -71,6 +110,29 @@ function selectSpike() {
         localStorage.setItem("imagensImportadas", JSON.stringify(arrayImagensImportadas));
     }
 }
+
+
+function objHandler(arrayImagens, arrayTagsImg) {
+    let listObj = []
+    for(var i = 0; i < arrayImagens.length(); i++){
+        filterImg = arrayTagsImg[i];
+        imgSrc = arrayImagens[i];      
+        var objToAdd = filterObj(imgSrc, filterImg)); 
+        listObj.push(objToAdd);
+
+        
+    }  
+    return (listObj) 
+}
+
+function filterObj(src, filterBoolean){
+
+    this.imgSrcObj = src;
+    this.praia = filterBoolean[0];
+    this.dia = filterBoolean[1];
+}
+
+
 
 function disableBackground() {
     $("#side-bar").addClass("disabled")
