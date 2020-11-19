e44744cad24e23ea11a9c3405dfc5a921de07094
mike12-debug <62181571+mike12-debug@users.noreply.github.com>
2020-11-19 21:24:16 +0000

    LOGIN IN THE MAKINGS
    checka a opacidade

--- scripts/login_pageScript.js
+++ scripts/login_pageScript.js
@@ -1,14 +1,61 @@
+const registerEmail = "email";
+const registerUsername = "username";
+const registerPassword = "password";
 
-var tempUsername = "admin";
-var tempPassword = "admin";
+const loginUsername = "username";
+const loginPassword = "password";
+
+
+
+
+function userAcc(username, password, email){
+
+    this.username = username;
+    this.password = password;
+    this.email = email;
+
+}
+
+
+function usernameGetter(){
+    return this.username;
+}
+
+
+function passGetter(){
+    return this.password;
+}
+
+
+
+function registerHandler(){
+    let registredAcc = [];
+    formGetter = document.forms.registerData;
+    registerUserData = formGetter.elements.username.value;
+    registerPassData= formGetter.elements.password.value;
+    registerEmailData= formGetter.elements.email.value;
+
+    let accToApend = new userAcc(registerUserData, registerPassData, registerEmailData);
+
+    registredAcc.push(accToApend);
+    
+    localStorage.setItem("accounts", JSON.stringify(registredAcc));
+
+}
 
 
 function loginHandler(){
     let formData = document.forms.loginData;
     dataUsername = formData.elements.username.value;
     dataPassword = formData.elements.password.value;
-    if(dataUsername == tempUsername && tempPassword == dataPassword){
-        location.replace("galeria.html");
+    let accToIterate = localStorage.getItem("accounts");
+    console.log(accToIterate)
+    for(let i = 0; accToIterate.length; i++){        
+        if(dataUsername == accToIterate[i].usernameGetter() && accToIterate[i].passGetter() == dataPassword){
+            location.replace("galeria.html");
+        }else{
+            console.log("Errado")
+        }
     }
 
 }
