b8f2d26bafed8df39729db959c831560a87ebb75
Francisco Miguel Mendes Pimenta <xisco.pimenta@gmail.com>
2020-12-04 22:22:44 +0000

    partilhar album DONE

--- scripts/partilhar_pageScripts.js
+++ scripts/partilhar_pageScripts.js
@@ -82,28 +82,64 @@ function open_icon_partilha(icon) {
 }
 
 function preencheTabelaAlbumPartilhar() {
+   
+    if (document.getElementById("botao-trocar-tipo").innerHTML == "Álbuns") {
 
-    document.getElementById("bot_row_icons").style.opacity = 1;
-    document.getElementById("top_row_icons").style.opacity = 1; 
-    $("#top_row_icons").removeClass("disabled");
-    $("#bot_row_icons").removeClass("disabled");
-    document.getElementById("p-importar").innerHTML = "Escolha o local para onde pretende partilhar as suas fotografias:";
+        document.getElementById("botao-trocar-tipo").innerHTML = "Fotografias"
+
+        if (localStorage.getItem("arrayImagensDiferentesAlbuns" + utilizador) != null) {
+            document.getElementById("texto-seleciona").innerHTML = "Selecione os álbuns que deseja partilhar:";
+            document.getElementById("texto-seleciona").style.left = "320px";
+
+            let imagensCapa = [];
+            let arrayAlbum = JSON.parse(localStorage.getItem("arrayImagensDiferentesAlbuns" + utilizador));
+            let nomesAlbum = JSON.parse(localStorage.getItem("nomesAlbums" + utilizador));
+            let tabela = document.querySelector("#tabela-fotos-partilha tbody");
+
+            tabela.innerHTML = "";
+
+            for (let album of arrayAlbum) {
+                let imgCapa = album[0];
+                imagensCapa.push(imgCapa);
+            }
+
+            let x = 0;
+            let i = 0;
+            let trElement;
     
-    if (localStorage.getItem("albumPartilhar" + utilizador) != null) {
-        document.getElementById("texto-seleciona").innerHTML = "Álbuns selecionados para partilhar:";
-        document.getElementById("texto-seleciona").style.left = "320px";
-        var arrayAlbum = JSON.parse(localStorage.getItem("fotosPartilhar" + utilizador));
-    } else {
-        var arrayAlbum = JSON.parse(localStorage.getItem("arrayImagensDiferentesAlbuns" + utilizador));
+            for (let imagemCapa of imagensCapa) {
+                    if(i%4 == 0 || x == 0){
+                        trElement = document.createElement('tr');
+                        x++;
+                    }
+    
+                    let linha = document.createElement("td");
+                    linha.innerHTML = "<label class='option-item-album'>" +
+                                        "<input type='checkbox' class='checkbox-album'>" +
+                                        "<div class='option-inner-album'>" +
+                                            "<img width='220px' height='140px' src='" + imagemCapa + "'>" +
+                                            nomesAlbum[i] +
+                                        "</div>" +
+                                    "</label>";
+                    trElement.appendChild(linha);
+                    tabela.appendChild(trElement);
+                    i++;
+    
+            }
+
+        } else {
+            document.getElementById("texto-seleciona").innerHTML = "Ainda não tem nenhum álbum. Crie um primeiro!";
+        }
+    } else { 
+        document.getElementById("texto-seleciona").innerHTML = "Selecione as fotografias que deseja partilhar:";
+        document.getElementById("botao-trocar-tipo").innerHTML = "Álbuns";
+        preencheTabelaImagensPartilhar(); 
     }
-
-    
-
 }
 
 function preencheTabelaImagensPartilhar() {
 
-    if (localStorage.getItem("imagensImportadas" + utilizador) != null) {
+    if (localStorage.getItem("imagensImportadas" + utilizador) != null || localStorage.getItem("imagensImportadas" + utilizador) != "[]") {
 
         document.getElementById("bot_row_icons").style.opacity = 1;
         document.getElementById("top_row_icons").style.opacity = 1; 
@@ -112,7 +148,7 @@ function preencheTabelaImagensPartilhar() {
         document.getElementById("p-importar").innerHTML = "Escolha o local para onde pretende partilhar as suas fotografias:"; 
 
         if (localStorage.getItem("fotosPartilhar" + utilizador) != null) {
-            document.getElementById("texto-seleciona").innerHTML = "Fotografias selecionadas para partilhar:";
+           c
             document.getElementById("texto-seleciona").style.left = "320px";
             var arrayImagens = JSON.parse(localStorage.getItem("fotosPartilhar" + utilizador));
         } else {
@@ -152,7 +188,6 @@ function preencheTabelaImagensPartilhar() {
         $("#top_row_icons").addClass("disabled");
         $("#bot_row_icons").addClass("disabled");
         }
-    document.getElementsByClassName("dimmer")[0].style.opacity="0"
 }
 
 
