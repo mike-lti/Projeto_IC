960258361f40b3d08be391bf6dd1557657c9a31a
FMPimenta <xisco.pimenta@gmail.com>
2020-12-11 23:25:51 +0000

    Update ajuda_pageScripts.js

--- scripts/ajuda_pageScripts.js
+++ scripts/ajuda_pageScripts.js
@@ -3,13 +3,14 @@
 var utilizador = localStorage.getItem("currentAccount").slice(1,localStorage.getItem("currentAccount").length -1);
 $(document).ready(currentAccPlacer());
 
-
+/* Coloca o nome do utilizador na sidebar */
 function currentAccPlacer(){
   var usernameLoggedInToParse = localStorage.getItem("currentAccount");
   var usernameLoggedIn = JSON.parse(usernameLoggedInToParse);
   document.getElementById("usernameCurrentAccount").innerHTML = usernameLoggedIn;
 }
 
+/* Faz o "dropup" do botão para sair da conta aparecer */
 function open_dropup() {
   if (document.getElementsByClassName("dropup-content")[0].style.display == "none" ||
     document.getElementsByClassName("dropup-content")[0].style.display == "") {
